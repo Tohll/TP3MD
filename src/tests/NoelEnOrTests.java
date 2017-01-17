f465c050d4c408e50465a72ec3d8867e4d72c12c
Tohll <le.tcharb@gmail.com>
2017-01-17 23:06:51 +0100

    test2

--- src/tests/NoelEnOrTests.java
+++ src/tests/NoelEnOrTests.java
@@ -10,7 +10,7 @@ public class NoelEnOrTests {
 	@Test
 	public void testNoel() {
 		
-		//classe de test artificielle (getProductList renvoi true du moment qu'elle marche!)
+		//classe de tests artificielle (getProductList renvoi true du moment qu'elle marche!)
 		ManagerProduits manager = new ManagerProduits();
 		Assert.assertTrue(manager.getProductsList());
 		
